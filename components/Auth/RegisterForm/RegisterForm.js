1599fc657ff0eae775ac0461aa837a3af8936243
pmNiko <nikolas090189@gmail.com>
2021-02-17 20:23:21 -0300

    Add form register

--- components/Auth/RegisterForm/RegisterForm.js
+++ components/Auth/RegisterForm/RegisterForm.js
@@ -1,10 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
+import { Form, Button } from "semantic-ui-react";
 
 export default function RegisterForm({ showLoginForm }) {
   return (
-    <div>
-      <h1>Estamos en el Registro</h1>
-      <button onClick={showLoginForm}>Ir al Login</button>
-    </div>
+    <Form className="login-form">
+      <Form.Input name="name" type="text" placeholder="Nombre" />
+      <Form.Input
+        name="lastname"
+        type="text"
+        placeholder="Apellido de usuario"
+      />
+      <Form.Input name="username" type="text" placeholder="Nombre de usuario" />
+      <Form.Input name="email" type="text" placeholder="Email de usuario" />
+      <Form.Input name="password" type="password" placeholder="Contraseña" />
+      <div className="actions">
+        <Button type="button" basic>
+          Iniciar sesion
+        </Button>
+        <Button type="submit" className="submit">
+          Registrar
+        </Button>
+      </div>
+    </Form>
   );
 }
