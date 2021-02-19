8623fc4f3c7155aa26bdb98e6f368376e4103014
pmNiko <nikolas090189@gmail.com>
2021-02-19 20:39:47 -0300

    Add style to form login

--- components/Auth/LoginForm/LoginForm.js
+++ components/Auth/LoginForm/LoginForm.js
@@ -1,10 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
+import { Form, Button } from "semantic-ui-react";
+import { useFormik } from "formik";
+import * as Yup from "yup";
+import { toats } from "react-toastify";
 
 export default function LoginForm({ showRegisterForm }) {
   return (
-    <div>
-      <h1>Estamos en el Login</h1>
-      <button onClick={showRegisterForm}>Ir al registro</button>
-    </div>
+    <Form className="login-form">
+      <Form.Input name="identifier" type="text" placeholder="Email" />
+      <Form.Input name="password" type="password" placeholder="password" />
+      <div className="actions">
+        <Button type="button" basic onClick={showRegisterForm}>
+          Registrarse
+        </Button>
+        <div>
+          <Button className="submit" type="submit">
+            Ingresar
+          </Button>
+          <Button type="button"> Has olvidado la contraseña? </Button>
+        </div>
+      </div>
+    </Form>
   );
 }
--- components/Auth/RegisterForm/RegisterForm.js
+++ components/Auth/RegisterForm/RegisterForm.js
@@ -63,7 +63,7 @@ export default function RegisterForm({ showLoginForm }) {
         error={formik.errors.password}
       />
       <div className="actions">
-        <Button type="button" basic>
+        <Button type="button" basic onClick={showLoginForm}>
           Iniciar sesion
         </Button>
         <Button type="submit" className="submit" loading={loading}>
