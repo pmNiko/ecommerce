09e5b0a4062491399422a8faf060cc5bd0f1a3bb
pmNiko <nikolas090189@gmail.com>
2021-02-19 21:37:58 -0300

    Login user ok

--- components/Auth/LoginForm/LoginForm.js
+++ components/Auth/LoginForm/LoginForm.js
@@ -2,14 +2,25 @@ import React, { useState } from "react";
 import { Form, Button } from "semantic-ui-react";
 import { useFormik } from "formik";
 import * as Yup from "yup";
-import { toats } from "react-toastify";
+import { toast, toats } from "react-toastify";
+import { loginApi } from "../../../api/user";
 
-export default function LoginForm({ showRegisterForm }) {
+export default function LoginForm({ showRegisterForm, onCloseModal }) {
+  const [loading, setLoading] = useState(false);
+
   const formik = useFormik({
     initialValues: initialValues(),
     validationSchema: Yup.object(validationSchema()),
-    onSubmit: (formData) => {
-      console.log(formData);
+    onSubmit: async (formData) => {
+      setLoading(true);
+      const response = await loginApi(formData);
+      if (response?.jwt) {
+        toast.success("Bienvenido...");
+        onCloseModal();
+      } else {
+        toast.error("Error al ingresar los datos.");
+      }
+      setLoading(false);
     },
   });
 
@@ -34,7 +45,7 @@ export default function LoginForm({ showRegisterForm }) {
           Registrarse
         </Button>
         <div>
-          <Button className="submit" type="submit">
+          <Button className="submit" type="submit" loading={loading}>
             Ingresar
           </Button>
           <Button type="button"> Has olvidado la contraseña? </Button>
